9ae5a0c0320b3317f143992e90e04ab894ba5258
kevinhwang95 <kevinhwang95@gmail.com>
2025-09-20 09:56:44 +0000

    add drawing and calculate area in thai rai

--- geocoding/src/App.tsx
+++ geocoding/src/App.tsx
@@ -1,6 +1,7 @@
 //import { Button } from "@/components/ui/button";
 import React, { useState } from 'react';
-import { APIProvider, Map, } from '@vis.gl/react-google-maps';
+//import { APIProvider, Map, } from '@vis.gl/react-google-maps';
+import MapWithPolygonDrawing from './components/ui/mymapcomponent';
 
 function App() {
   const [clickedLocation, setClickedLocation] = useState<google.maps.LatLngLiteral | null>(null);
@@ -11,22 +12,23 @@ function App() {
     }
   };
   return (
-    <APIProvider apiKey={'AIzaSyBdsqAGgmfJQ3-vZhL9qPUSIVhqciANlpY'} onLoad={() => console.log('Maps API has loaded.')}>
-      <div style={{ height: '500px', width: '100%' }}>
-        <Map
-          defaultCenter={{ lat:34, lng: -118.243683 }} // Example default center (Los Angeles)
-          defaultZoom={10}
-          onMapClick={handleMapClick}
-        />
-      </div>
-      {clickedLocation && (
-        <div>
-          <p>Last clicked location:</p>
-          <p>Latitude: {clickedLocation.lat}</p>
-          <p>Longitude: {clickedLocation.lng}</p>
-        </div>
-      )}
-    </APIProvider>
+    // <APIProvider apiKey={'AIzaSyBdsqAGgmfJQ3-vZhL9qPUSIVhqciANlpY'} onLoad={() => console.log('Maps API has loaded.')}>
+    //   <div style={{ height: '100%', width: '100%' }}>
+    //     <Map
+    //       defaultCenter={{ lat:14.09480, lng: 99.82120 }} // Example default center (Los Angeles)
+    //       defaultZoom={18}
+    //       onMapClick={handleMapClick}
+    //     />
+    //   </div>
+    //   {clickedLocation && (
+    //     <div>
+    //       <p>Last clicked location:</p>
+    //       <p>Latitude: {clickedLocation.lat}</p>
+    //       <p>Longitude: {clickedLocation.lng}</p>
+    //     </div>
+    //   )}
+    // </APIProvider>
+    <MapWithPolygonDrawing />
   )
 }
 
